341fcc4a7adcbf80ccf37990400ef31f0fe8850b
Marcin Moch <marcio.moch@gmail.com>
2021-05-28 23:30:52 +0200

    frontend init

---
+++ packages/frontend/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+const Application: React.FunctionComponent<{}> = () => <h1>Application</h1>;
+
+render(<Application />, document.getElementById('root'));
