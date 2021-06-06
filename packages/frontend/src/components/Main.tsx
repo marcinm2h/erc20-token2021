381f0347865a3a986f4bb42e579be95fc0e73567
Marcin Moch <marcio.moch@gmail.com>
2021-06-06 22:47:38 +0300

    Contract interaction

--- packages/frontend/src/components/Main.tsx
+++ packages/frontend/src/components/Main.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import MetaMaskOnboarding from '@metamask/onboarding';
+import Token2021 from 'contract/build/contracts/Token2021.json';
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
 
 const { isMetaMaskInstalled } = MetaMaskOnboarding;
 
-let ethersProvider;
-// let hstFactory
+Object.assign(window, { isMetaMaskInstalled });
 
 const currentUrl = new URL(window.location.href);
 const forwarderOrigin =
@@ -20,7 +26,7 @@ Object.assign(window, {
 // https://github.com/MetaMask/test-dapp
 const initialize = async () => {
   try {
-    ethersProvider = new ethers.providers.Web3Provider(window.ethereum, 'any');
+    // ethersProvider = new ethers.providers.Web3Provider(window.ethereum, 'any');
     // hstFactory = new ethers.ContractFactory(
     //   hstAbi,
     //   hstBytecode,
@@ -30,15 +36,14 @@ const initialize = async () => {
     console.error(error);
   }
 
-  let onboarding;
+  let onboarding: MetaMaskOnboarding;
   try {
     onboarding = new MetaMaskOnboarding({ forwarderOrigin });
   } catch (error) {
     console.error(error);
   }
 
-  let accounts;
-  let accountButtonsInitialized = false;
+  let accounts: string[];
 
   const isMetaMaskConnected = () => accounts && accounts.length > 0;
 
@@ -46,72 +51,106 @@ const initialize = async () => {
     onboarding.startOnboarding();
   };
 
-  const connect = async () => {
-    try {
-      const newAccounts = await ethereum.request({
-        method: 'eth_requestAccounts',
-      });
-      handleNewAccounts(newAccounts);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  function handleNewAccounts(newAccounts) {
-    accounts = newAccounts;
-    accountsDiv.innerHTML = accounts;
-    if (isMetaMaskConnected()) {
-      initializeAccountButtons();
-    }
-    updateButtons();
-  }
-
-  function handleNewChain(chainId) {
-    chainIdDiv.innerHTML = chainId;
-  }
-
-  function handleNewNetwork(networkId) {
-    networkDiv.innerHTML = networkId;
-  }
-
   async function getNetworkAndChainId() {
     try {
-      const chainId = await ethereum.request({
+      const chainId = await window.ethereum.request({
         method: 'eth_chainId',
       });
-      handleNewChain(chainId);
+      // handleNewChain(chainId);
 
-      const networkId = await ethereum.request({
+      const networkId = await window.ethereum.request({
         method: 'net_version',
       });
-      handleNewNetwork(networkId);
+      // handleNewNetwork(networkId);
     } catch (err) {
       console.error(err);
     }
   }
 
   if (isMetaMaskInstalled()) {
-    ethereum.autoRefreshOnNetworkChange = false;
+    window.ethereum.autoRefreshOnNetworkChange = false;
     getNetworkAndChainId();
 
-    // ethereum.on('chainChanged', handleNewChain)
-    // ethereum.on('networkChanged', handleNewNetwork)
-    ethereum.on('accountsChanged', handleNewAccounts);
+    // window.ethereum.on('chainChanged', handleNewChain)
+    // window.ethereum.on('networkChanged', handleNewNetwork)
+    // window.ethereum.on('accountsChanged', handleNewAccounts);
 
     try {
-      const newAccounts = await ethereum.request({
+      const newAccounts = await window.ethereum.request({
         method: 'eth_accounts',
       });
-      handleNewAccounts(newAccounts);
+      // handleNewAccounts(newAccounts);
     } catch (err) {
       console.error('Error on init when getting accounts', err);
     }
   }
 };
 
+const ethersProvider = new ethers.providers.Web3Provider(
+  window.ethereum,
+  'any'
+);
+
+const contractAddress = '0xd8EC4F3364A6d6A7a68F0FE6705E9AA92aa230b4'; // rinkeby
+
+window.contract = new ethers.Contract(
+  contractAddress,
+  Token2021.abi,
+  ethersProvider
+);
+
+window.provider = ethersProvider;
+
+window.ethers = ethers;
+
+const claim = async () => {
+  // useProvider();
+  const ethersProvider = new ethers.providers.Web3Provider(
+    window.ethereum,
+    'any'
+  );
+  // ethersProvider.getSigner().getAddress().then(x => console.log({x}))
+
+  // process.env.TOKEN2021_CONTRACT_ADDRESS
+  const contractAddress = '0xd8EC4F3364A6d6A7a68F0FE6705E9AA92aa230b4'; // rinkeby
+
+  const contract = new ethers.Contract(
+    contractAddress,
+    Token2021.abi,
+    ethersProvider.getSigner()
+  );
+
+  await contract.claim();
+};
+
+const balanceOf = async (
+  account: string = '0x4431a11dcc45ada7e82985b2de3889c86fcb544f'
+) => {
+  // useProvider();
+  const ethersProvider = new ethers.providers.Web3Provider(
+    window.ethereum,
+    'any'
+  );
+
+  // process.env.TOKEN2021_CONTRACT_ADDRESS
+  const contractAddress = '0xd8EC4F3364A6d6A7a68F0FE6705E9AA92aa230b4'; // rinkeby
+
+  const contract = new ethers.Contract(
+    contractAddress,
+    Token2021.abi,
+    ethersProvider
+  );
+
+  const balance = (await contract.balanceOf(account)).toNumber();
+
+  return balance;
+};
+
 const connect = async () => {
+  window.ethereum.on('accountsChanged', (x) => console.log(x));
+
   try {
-    const newAccounts = await ethereum.request({
+    const newAccounts = await window.ethereum.request({
       method: 'eth_requestAccounts',
     });
     console.log(newAccounts);
@@ -123,26 +162,41 @@ const connect = async () => {
   }
 };
 
-// function handleNewAccounts(newAccounts) {
-//   if (isMetaMaskConnected()) {
-//     initializeAccountButtons()
-//   }
-//   updateButtons()
-// }
-
-console.log(process.env.WEBSITE_TITLE);
-
 export const Main: React.FC<any> = () => {
   const [connected, setConnected] = useState(false);
   const [accounts, setAccounts] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [balance, setBalance] = useState(null);
+
+  useEffect(() => {
+    window.ethereum.on('accountsChanged', setAccounts);
+  }, []);
 
   return (
     <div>
       <h1>Main</h1>
       <hr />
       <div>
+        <button
+          type="button"
+          onClick={(e) => {
+            claim();
+          }}
+        >
+          claim
+        </button>
+        <button
+          type="button"
+          onClick={(e) => {
+            balanceOf(accounts[0]).then((balance) => {
+              setBalance(balance);
+            });
+          }}
+        >
+          Get my balance
+        </button>
+        {balance !== null && <pre>balance: {balance}</pre>}
         <button
           type="button"
           disabled={connected || loading}
